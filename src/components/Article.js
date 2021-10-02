3334dd94b8e2ddccc72c4e8f76679825488354ab
Alessandra Chardon <achardon@ucla.edu>
2021-10-01 21:16:04 -0400

    added bonus feature

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,12 +3,27 @@ import React from "react";
 function Article({ 
     title, 
     date='January 1, 1970', 
-    preview 
+    preview,
+    minutes
 }) {
+    let minRead = ''
+    if (minutes < 30) {
+        for (let i=0; i<minutes; i=i+5) {
+            minRead = minRead + '☕️'
+        }
+    }
+        
+    if (minutes > 30) {
+        for (let i=0; i<minutes; i=i+10) {
+            minRead = minRead + '🍱'
+        }
+    }
+    
+    
     return (
         <article>
             <h3>{title}</h3>
-            <small>{date}</small>
+            <small>{date} ~ {minRead} {minutes} min read</small>
             <p>{preview}</p>
         </article>
     )
